a11da844f419f2ebd0c84964e64e80e4ea16a516
andrewspalato <7651775@student.swin.edu.au>
2016-08-21 21:29:49 +1000

    Saves bgm volume via player prefs.

---
+++ ContinuumReboot/Assets/Scripts/FadeFilterAudio.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeFilterAudio : MonoBehaviour 
+{
+	public AudioLowPassFilter LowPassFilter;
+	public float filterSpeed = 10.0f;
+
+	void Start () 
+	{
+		LowPassFilter.enabled = true;
+	}
+	
+	void Update () 
+	{
+		LowPassFilter.cutoffFrequency += filterSpeed * Time.deltaTime;
+		if (LowPassFilter.cutoffFrequency > 20000) 
+		{
+			GetComponent<FadeFilterAudio> ().enabled = false;
+		}
+	
+	}
+}
---
+++ ContinuumReboot/Assets/Scripts/SaveAudioVolume.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SaveAudioVolume : MonoBehaviour 
+{
+	public Slider BGMVolSlider;
+	public AudioSource BGMAudio;
+
+	void Start () 
+	{
+		BGMAudio.volume =  PlayerPrefs.GetFloat ("BGMVol", 1);
+		BGMVolSlider.value = PlayerPrefs.GetFloat ("BGMVol", 1);
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	public void OnValueChanged () 
+	{
+		PlayerPrefs.SetFloat ("BGMVol", BGMVolSlider.value);
+	}
+}
