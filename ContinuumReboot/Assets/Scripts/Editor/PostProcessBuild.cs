d0bfefaefc49c7c2a63c6a92a56b9022463ee743
andrewspalato <7651775@student.swin.edu.au>
2016-08-23 23:30:36 +1000

    Added editor script to delete annoying .pdb files on build with windows.

---
+++ ContinuumReboot/Assets/Scripts/Editor/PostProcessBuild.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.IO;
+
+public class PostProcessBuild 
+{
+	[PostProcessBuildAttribute(1)]
+	public static void OnPostProcessBuild (BuildTarget target, string pathToBuiltProject)
+	{
+		if (target == BuildTarget.StandaloneWindows || target == BuildTarget.StandaloneWindows64) 
+		{
+			// Removes annoying .pdb files on build.
+			string pureBuildPath = Path.GetDirectoryName (pathToBuiltProject);
+			foreach (string file in Directory.GetFiles(pureBuildPath, "*.pdb")) 
+			{
+				Debug.Log (file + " deleted!");
+				File.Delete (file);
+			}
+		}
+	}
+}
