24d15d0bed9bfda8a154a0b32f8d22d26e211e89
andrewspalato <7651775@student.swin.edu.au>
2016-10-09 19:53:43 +1100

    Bosses have names

--- ContinuumReboot/Assets/Scripts/EnemyController.cs
+++ ContinuumReboot/Assets/Scripts/EnemyController.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class EnemyController : MonoBehaviour 
 {
 	public GameObject Brain;
 	public GameObject bossExplosion;
+	public string bossName;
+	public Text NameText;
+	public Animator NameAnim;
 	private GameObject boss;
 	private Transform bossHover;
 	private SmoothFollowOrig smoothFollowScript;
@@ -15,10 +19,15 @@ public class EnemyController : MonoBehaviour
 		gameControllerScript = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController>();
 		Brain = GameObject.Find ("Brain");
 		boss = GameObject.FindGameObjectWithTag ("Boss");
-		bossHover = GameObject.Find ("EnemyBossRestPoint").GetComponent<Transform>();
+		bossHover = GameObject.Find ("EnemyBossRestPoint").GetComponent<Transform> ();
 
 		smoothFollowScript = GetComponent<SmoothFollowOrig> ();
 		smoothFollowScript.target = bossHover.transform;
+
+		NameText = GameObject.Find ("BossText").GetComponent<Text> ();
+		NameAnim = GameObject.Find ("BossText").GetComponent<Animator> ();
+		NameText.text = "" + bossName + "";
+		NameAnim.Play (0);
 	}
 
 	void Update () 
