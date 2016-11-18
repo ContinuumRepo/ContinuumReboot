65da89e29f2a35d3883ecef95dbd3dd50d3f9911
andrewspalato <7651775@student.swin.edu.au>
2016-11-18 22:26:17 +1100

    Started on new menu

---
+++ ContinuumReboot/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		//GetComponent<Transform> ().position = new Vector3 (0.2f, 2.1f, -165.1f);
+		GetComponent<Transform> ().rotation = Quaternion.Euler (0, 0, -135);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- ContinuumReboot/Assets/Scripts/OLD/RotateByValues.cs
+++ ContinuumReboot/Assets/Scripts/OLD/RotateByValues.cs
@@ -5,6 +5,8 @@ public class RotateByValues : MonoBehaviour
 {
 	public float amount;
 	public Vector3 offset;
+	public enum axis {X, Y, Z}
+	public axis Axis;
 
 	void Start () 
 	{
@@ -13,11 +15,34 @@ public class RotateByValues : MonoBehaviour
 
 	void FixedUpdate () 
 	{
-		gameObject.transform.localRotation = Quaternion.Euler 
+		if (Axis == axis.X) 
+		{
+			gameObject.transform.localRotation = Quaternion.Euler 
+				(
+					(transform.localPosition.x * amount) + offset.x,
+					offset.y,
+					offset.z 
+				);
+		}
+
+		if (Axis == axis.Y) 
+		{
+			gameObject.transform.localRotation = Quaternion.Euler 
+				(
+					offset.x, 
+					(transform.localPosition.x * amount) + offset.y,
+					offset.z 
+				);
+		}
+
+		if (Axis == axis.Z) 
+		{
+			gameObject.transform.localRotation = Quaternion.Euler 
 			(
 				offset.x, 
 				offset.y, 
-				(transform.localPosition.x * amount) + offset.z
+				(transform.localPosition.x * amount) + offset.x
 			);
+		}
 	}
 }
