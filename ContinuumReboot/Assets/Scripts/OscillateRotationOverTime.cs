a7ebfb1243b10335be0ad9c51ea2c5b6bc3934ef
andrewspalato <7651775@student.swin.edu.au>
2016-11-25 18:47:05 +1100

    Updated game controller to change fixed time step proportionally to time scale Added application.target framerate to -1 meaning "As fast as it can render" (for some reason some devices lock at 30fps on default). Tweaked the menu some more.

---
+++ ContinuumReboot/Assets/Scripts/OscillateRotationOverTime.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OscillateRotationOverTime : MonoBehaviour 
+{
+	public float amount = 1;
+	public float frequency = 1;
+	public float time;
+	public float offset;
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		time += Time.deltaTime;
+
+		transform.rotation = Quaternion.Euler(0, amount * Mathf.Sin(time * frequency) + offset, 0);
+	}
+}
