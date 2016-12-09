2133c2db4d09370ec3b1dadcf2e5cb556a179b4e
andrewspalato <7651775@student.swin.edu.au>
2016-12-09 21:22:27 +1100

    Timescale controller is nice and neatly coded.

--- ContinuumReboot/Assets/Scripts/TimescaleController.cs
+++ ContinuumReboot/Assets/Scripts/TimescaleController.cs
@@ -5,67 +5,85 @@ using UnityEngine.UI;
 
 public class TimescaleController : MonoBehaviour 
 {
-	public float timeScaleReadOnly;			// The actual value of Time.timeScale.
-
+	
+	public calcMode CalculationMode;
 	public enum calcMode
 	{
-		Distance, 							// Calculates music pitch by via distance
-		Continuous,
 		timeScale,							// Calculates music pitch by Time.timeScale directly
 		none								// Does nothing to the music pitch.
 	}
 
-	public calcMode CalculationMode;
-
-	public enum mode
-	{
-		onePlayer, 
-		//twoPlayers, 
-		//threePlayers, 
-		//fourPlayers
-	}
-
+	public enum mode {onePlayer}
 	public mode PlayerMode;
 
-	public float highestTimeScale; 			// For stats screen.
-
+	[Header ("Time settings")]
+	// Time settings.
+	public float timeScaleReadOnly;			// The actual value of Time.timeScale.
 	public float startTimeScale = 1.0f; 	// So there is no runtime error that timescale is below 0 at the start of the game.
 	public float timeSpeedIncreaseSens;		// Multipler to minimum time increase per frame.
 	public float addMinTime = 0.1f; 		// Minimum time scale.
 	public float timeSpeedSens; 			// Dampens change in timescale as the distance between player and reference point increase.
-	public float distance;					// y-distance from Reference point to player.
-	public Text MultiplierText;				// Multipler text UI in %.
+	private float distance;					// y-distance from Reference point to player.
 	private float currentTimeScale; 		// Stores calculation of timescale here.
 	private Transform referencePoint;		// Needs this to calculate.
-	public AudioSource Music;				// The main game music.
+	public Text MultiplierText;				// Multipler text UI in %.
 
+	[Header ("Music layers")]
+	// The layered soundtracks to add in inspector.
 	public AudioSource BassDrums;
 	public AudioSource Synth1;
 	public AudioSource Synth2;
 	public AudioSource Synth3;
 	public AudioSource Riff;
 
+	[Header ("Volume interpolation")]
+	// Linear interpolating volume.
 	public float lerpBassVol;
 	public float lerpSynth1Vol;
 	public float lerpSynth2Vol;
 	public float lerpSynth3Vol;
 	public float lerpRiffVol;
 
+	[Header ("Pitch interpolation")]
+	// Linear interpolating pitch.
 	public float lerpBassPitch;
 	public float lerpSynth1Pitch;
 	public float lerpSynth2Pitch;
 	public float lerpSynth3Pitch;
 	public float lerpRiffPitch;
 
-	public ParticleSystem DistantStars;		// The star field particles.
+	[Header ("MISC")]
+	// MISC.
 	public MeshRenderer PlayerMat; 			// The player material.
-
 	private Transform playerOne;			// Player one transform.
+
+	[Header ("Volume tweak values")]
+	[Range (0, 1f)]
+	public float lowVolSetting = 0.0f;
+	[Range (0, 1)]
+	public float mediumVolSetting = 0.5f;
+	[Range (0, 1)]
+	public float highVolSetting = 1.0f;
+
+	[Header ("Pitch tweak values")]
+	public float floorPitchSetting = 0.1f;
+	public float lowPitchSetting = 0.4f;
+	public float mediumPitchSetting = 1.0f;
+	public float medhighPitchSetting = 1.1f;
+	public float highPitchSetting = 1.25f;
+	public float roofPitchSetting = 1.5f;
+
+	[Header ("Time.timeScale checkpoints")]
+	public float time0Layers = 0.5f;
+	public float time1Layers = 1.0f;
+	public float time2Layers = 2.0f;
+	public float time3Layers = 3.0f;
+	public float timeRiffLayers = 4.0f;
 
 	public void Start () 
 	{
-		Time.timeScale = startTimeScale; // Sets start Time.timeScale.
-		CalculationMode = calcMode.none; // Starts music pitch mode on none.
+		Time.timeScale = startTimeScale; 	// Sets start Time.timeScale.
+		CalculationMode = calcMode.none;	// Starts music pitch mode on none.
 
 		// Finds reference point gameObject.transform.
 		referencePoint = GameObject.FindGameObjectWithTag ("ReferencePoint").transform; 
@@ -76,31 +94,29 @@ public class TimescaleController : MonoBehaviour
 			playerOne = GameObject.Find ("Player").transform; 
 		}
 	}
-
-	public void Update () 
+		
+	public void Update ()
 	{	
-		BassDrums.volume = Mathf.Lerp (BassDrums.volume, lerpBassVol, 2 * Time.unscaledDeltaTime);
-		Synth1.volume = Mathf.Lerp (Synth1.volume, lerpSynth1Vol, 2 * Time.unscaledDeltaTime);
-		Synth2.volume = Mathf.Lerp (Synth2.volume, lerpSynth2Vol, 2 * Time.unscaledDeltaTime);
-		Synth3.volume = Mathf.Lerp (Synth3.volume, lerpSynth3Vol, 2 * Time.unscaledDeltaTime);
-		Riff.volume = Mathf.Lerp (Riff.volume, lerpRiffVol, 2 * Time.unscaledDeltaTime);
+		AssignVolAndPitch ();
+		RefreshTimeScale ();
 
-		BassDrums.pitch = Mathf.Lerp (BassDrums.pitch, lerpBassPitch, 2 * Time.unscaledDeltaTime);
+		if (PlayerMode == mode.onePlayer) 
+		{
+			CalculateDistance ();
+		}
 
-		//Music.pitch = Mathf.Lerp (Music.pitch, lerpPitch, 2 * Time.unscaledDeltaTime);
-		Time.timeScale = Mathf.Clamp (Time.timeScale, 0.05f, 10.0f);
-
-		if (Time.timeScale <= 0) 
+		if (Time.timeScale <= Time.unscaledDeltaTime) 
 		{
 			Time.timeScale = 0.001f;
 		}
 			
+		// This mode occurs when the player GameObject is hit and recovering.
 		if (CalculationMode == calcMode.none) 
 		{
 			if (Time.timeScale < 1) 
 			{
 				Time.timeScale += 0.2f * Time.unscaledDeltaTime;
-				Music.pitch = Time.timeScale;
+				BassDrums.pitch = Time.timeScale;
 			}
 
 			if (Time.timeScale >= 1) 
@@ -108,180 +124,105 @@ public class TimescaleController : MonoBehaviour
 				CalculationMode = calcMode.timeScale;
 			}
 		}
-
-		if (CalculationMode == calcMode.Continuous) 
-		{
-			distance = playerOne.transform.position.y - referencePoint.transform.position.y;
-			Time.timeScale = distance;
-			Music.pitch = Time.timeScale;
-
-			// Stores the highest timescale value for stats.
-			if (Time.timeScale > highestTimeScale) {
-				highestTimeScale = Time.timeScale;
-			}
-
-			if (Time.timeScale < 0.5f) 
-			{
-				//lerpPitch = 0.15f;
-			}
-
-			if (Time.timeScale >= 0.5f && Time.timeScale < 1f) 
-			{
-				//lerpPitch = 0.4f;
-			}
-
-			if (Time.timeScale >= 1f && Time.timeScale < 2f) 
-			{
-				//lerpPitch = 1f;
-			}
-
-			if (Time.timeScale >= 2f && Time.timeScale < 3f) 
-			{
-				//lerpPitch = 1.25f;
-			}
-
-			if (Time.timeScale >= 3f && Time.timeScale < 4f) 
-			{
-				//lerpPitch = 1.5f;
-			}
-
-			if (Time.timeScale >= 4f) 
-			{
-				//lerpPitch = 2f;
-			}
-		}
-
-		// See actual time.TimeScale in inspector so you dont have to always check in edit > Project Settings > Time.
-		timeScaleReadOnly = Time.timeScale;
-
-		if (PlayerMode == mode.onePlayer) 
-		{
-			distance = playerOne.transform.position.y - referencePoint.transform.position.y;
-			Time.timeScale = ((distance + currentTimeScale) * timeSpeedSens) + addMinTime;
-			currentTimeScale += Time.unscaledDeltaTime * timeSpeedIncreaseSens;
-		}
 			
+		// Calculates pitch and volume of each layer of soundtrack.
 		if (CalculationMode == calcMode.timeScale) 
 		{
-			// Stores the highest timescale value for stats.
-			if (Time.timeScale > highestTimeScale)
+			if (Time.timeScale < time0Layers) 
 			{
-				highestTimeScale = Time.timeScale;
+				lerpBassPitch = floorPitchSetting;
+
+				lerpBassVol = mediumVolSetting;
+				lerpSynth1Vol = lowVolSetting;
+				lerpSynth2Vol = lowVolSetting;
+				lerpSynth3Vol = lowVolSetting;
+				lerpRiffVol = lowVolSetting;
 			}
 
-			if (Time.timeScale < 0.5f) 
+			if (Time.timeScale >= time0Layers && Time.timeScale < time1Layers) 
 			{
-				lerpBassPitch = 0.1f;
+				lerpBassPitch = lowPitchSetting;
 
-				lerpBassVol = 0.5f;
-				lerpSynth1Vol = 0.0f;
-				lerpSynth2Vol = 0.0f;
-				lerpSynth3Vol = 0.0f;
-				lerpRiffVol = 0.0f;
+				lerpBassVol = highVolSetting;
+				lerpSynth1Vol = mediumVolSetting;
+				lerpSynth2Vol = lowVolSetting;
+				lerpSynth3Vol = lowVolSetting;
+				lerpRiffVol = lowVolSetting;
 			}
 
-			if (Time.timeScale >= 0.5f && Time.timeScale < 1f) 
+			if (Time.timeScale >= time1Layers && Time.timeScale < time2Layers) 
 			{
-				lerpBassPitch = 0.4f;
+				lerpBassPitch = mediumPitchSetting;
 
-				lerpBassVol = 1.0f;
-				lerpSynth1Vol = 0.5f;
-				lerpSynth2Vol = 0.0f;
-				lerpSynth3Vol = 0.0f;
-				lerpRiffVol = 0.0f;
+				lerpBassVol = highVolSetting;
+				lerpSynth1Vol = highVolSetting;
+				lerpSynth2Vol = mediumVolSetting;
+				lerpSynth3Vol = lowVolSetting;
+				lerpRiffVol = lowVolSetting;
 			}
 
-			if (Time.timeScale >= 1f && Time.timeScale < 2f) 
+			if (Time.timeScale >= time1Layers && Time.timeScale < time3Layers) 
 			{
-				lerpBassPitch = 1f;
+				lerpBassPitch = medhighPitchSetting;
 
-				lerpBassVol = 1.0f;
-				lerpSynth1Vol = 1f;
-				lerpSynth2Vol = 0.5f;
-				lerpSynth3Vol = 0.0f;
-				lerpRiffVol = 0.01f;
+				lerpBassVol = highVolSetting;
+				lerpSynth1Vol = highVolSetting;
+				lerpSynth2Vol = highVolSetting;
+				lerpSynth3Vol = mediumVolSetting;
+				lerpRiffVol = lowVolSetting;
 			}
 
-			if (Time.timeScale >= 2f && Time.timeScale < 3f) 
+			if (Time.timeScale >= time3Layers && Time.timeScale < timeRiffLayers) 
 			{
-				lerpBassPitch = 1.1f;
+				lerpBassPitch = highPitchSetting;
 
-				lerpBassVol = 1.0f;
-				lerpSynth1Vol = 1f;
-				lerpSynth2Vol = 1f;
-				lerpSynth3Vol = 0.5f;
-				lerpRiffVol = 0.0f;
+				lerpBassVol = highVolSetting;
+				lerpSynth1Vol = highVolSetting;
+				lerpSynth2Vol = highVolSetting;
+				lerpSynth3Vol = highVolSetting;
+				lerpRiffVol = mediumVolSetting;
 			}
 
-			if (Time.timeScale >= 3f && Time.timeScale < 4f) 
+			if (Time.timeScale >= timeRiffLayers) 
 			{
-				lerpBassPitch = 1.25f;
+				lerpBassPitch = roofPitchSetting;
 
-				lerpBassVol = 1.0f;
-				lerpSynth1Vol = 1f;
-				lerpSynth2Vol = 1f;
-				lerpSynth3Vol = 1f;
-				lerpRiffVol = 0.5f;
-			}
-
-			if (Time.timeScale >= 4f) 
-			{
-				lerpBassPitch = 1.5f;
-
-				lerpBassVol = 1.0f;
-				lerpSynth1Vol = 1.0f;
-				lerpSynth2Vol = 1.0f;
-				lerpSynth3Vol = 1.0f;
-				lerpRiffVol = 1.0f;
+				lerpBassVol = highVolSetting;
+				lerpSynth1Vol = highVolSetting;
+				lerpSynth2Vol = highVolSetting;
+				lerpSynth3Vol = highVolSetting;
+				lerpRiffVol = highVolSetting;
 			}
 		}
+	}
 
-		if (CalculationMode == calcMode.Distance) 
-		{
-			if (distance > 0)
-			{
-				Time.timeScale = ((distance + currentTimeScale) * timeSpeedSens) + addMinTime; // Stores values into time.TimeScale.
-				currentTimeScale += Time.unscaledDeltaTime * timeSpeedIncreaseSens; // Increases minimum timescale.
-			}
+	void CalculateDistance ()
+	{
+		distance = playerOne.transform.position.y - referencePoint.transform.position.y;
+		Time.timeScale = ((distance + currentTimeScale) * (0.01f * timeSpeedSens)) + addMinTime;
+		currentTimeScale += Time.unscaledDeltaTime * (0.01f * timeSpeedIncreaseSens);
+	}
 
-			//Music.pitch = Time.timeScale;
-			if (distance < 7) 
-			{
-				//lerpPitch = 0.25f;
-			}
+	void RefreshTimeScale ()
+	{
+		// See actual time.TimeScale in inspector so you dont have to always check in 
+		// edit -> 
+		//		  Project Settings 
+		//						  -> Time.
+		timeScaleReadOnly = Time.timeScale;
+	}
 
-			if (distance < 14 && distance >= 7) 
-			{
-				if (Time.timeScale < 1 || Music.pitch < 1)
-				{
-					//lerpPitch = 0.5f;
-				}
+	void AssignVolAndPitch ()
+	{
+		// Volume changes on soundtracks separately.
+		BassDrums.volume = Mathf.Lerp (BassDrums.volume, lerpBassVol, 2 * Time.unscaledDeltaTime);
+		Synth1.volume = Mathf.Lerp (Synth1.volume, lerpSynth1Vol, 2 * Time.unscaledDeltaTime);
+		Synth2.volume = Mathf.Lerp (Synth2.volume, lerpSynth2Vol, 2 * Time.unscaledDeltaTime);
+		Synth3.volume = Mathf.Lerp (Synth3.volume, lerpSynth3Vol, 2 * Time.unscaledDeltaTime);
+		Riff.volume = Mathf.Lerp (Riff.volume, lerpRiffVol, 2 * Time.unscaledDeltaTime);
 
-				if (Time.timeScale >= 1 || Music.pitch > 1) 
-				{
-					//lerpPitch = 1;
-				}
-			}
-
-			if (distance >= 14 && distance < 21) 
-			{
-				//lerpPitch = 1f;
-			}
-
-			if (distance >= 21 && distance < 28) 
-			{
-				//lerpPitch = 1.25f;
-			}
-
-			if (distance >= 28 && distance < 35) 
-			{
-				//lerpPitch = 1.5f;
-			}
-
-			if (distance >= 35) 
-			{
-				//lerpPitch = 1.75f;
-			}
-		}
+		// Only have to change pitch on the bass, the other synth soundtracks will follow if 
+		// they have the PitchSync script attached.
+		BassDrums.pitch = Mathf.Lerp (BassDrums.pitch, lerpBassPitch, 2 * Time.unscaledDeltaTime);
 	}
 }
